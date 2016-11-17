0f907d08559583e6ad472e9ae94d292a663ee42d
Sofia Liila <sofia.liila@edu.hel.fi>
2016-11-17 15:24:31 +0200

    Collectible modified

---
+++ Assets/damagePlayer.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class damagePlayer : MonoBehaviour {
+	public int playerHealth = 10;
+	int damage = 10;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	void OnCollisionEnter(Collision _Collision)
+	if(Collision.GameObject.tag == "enemy"){
+}
